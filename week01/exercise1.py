fc9070f8595c5623bd301c49d468de783429f8bd
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-23 13:28:42 +1000

    Complete count_occurences function

--- week01/exercise1.py
+++ week01/exercise1.py
@@ -1,12 +1,20 @@
+WORDS = ["I", "really", "like", "legumes", "such", "as", "beans", "and", "peas", "mmm", "beans"]
+
+
 def count_occurrences(word_list, target_word):
     """
-    This function counts the number of times a word appears in a list
+    Count the number of times a word appears in a list
     :param word_list:
     :param target_word:
     :return:
     """
-    pass
+    occurrences = 0
+    for word in word_list:
+        if word == target_word:
+            occurrences += 1
+    return occurrences
 
 
-count_occurrences()
-count_occurrences()
+print(count_occurrences(WORDS, "legumes"))  # Should be 1
+print(count_occurrences(WORDS, "beans"))  # Should be 2
+print(count_occurrences(WORDS, "existential"))  # Should be 0
