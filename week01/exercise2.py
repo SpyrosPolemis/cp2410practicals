b2e5893a68d33318b966c8057519e83a10ab626d
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-23 13:30:52 +1000

    Initial commit, sample code

---
+++ week01/exercise2.py
@@ -0,0 +1,7 @@
+def find_max(numbers):
+    """Return max number in a list of numbers."""
+    max_number = ____
+    for number in ____:
+        if ____ > max_number:
+            max_number = ____
+    return max_number
