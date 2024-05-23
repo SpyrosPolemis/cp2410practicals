fe27dfeb8b57ba96ad2c51bbda2bd837694ade08
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-23 13:42:01 +1000

    Writ find_max function and tests

--- week01/exercise2.py
+++ week01/exercise2.py
@@ -1,7 +1,16 @@
+from random import randint
+
+
 def find_max(numbers):
-    """Return max number in a list of numbers."""
-    max_number = ____
-    for number in ____:
-        if ____ > max_number:
-            max_number = ____
+    """Return max number in a list of numbers. O(n)"""
+    max_number = 0
+    for number in numbers:
+        if number > max_number:
+            max_number = number
     return max_number
+
+
+print(find_max([]))  # 0
+print(find_max([2, 3, 5, 7, 11]))  # 11
+print(find_max([5, 5, 5, 5, 5, 10]))  # 10
+print(find_max([randint(0, 100) for i in range(10000)]))  #
