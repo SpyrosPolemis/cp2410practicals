d0334a732b1fec4cd70c03b8455735f46ed60f58
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-23 13:13:51 +1000

    Initial commit, sample code

---
+++ week01/exercise1.py
@@ -0,0 +1,12 @@
+def count_occurrences(word_list, target_word):
+    """
+    This function counts the number of times a word appears in a list
+    :param word_list:
+    :param target_word:
+    :return:
+    """
+    pass
+
+
+count_occurrences()
+count_occurrences()
