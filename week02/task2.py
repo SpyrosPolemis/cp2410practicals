fad29acc75f69150d440c0dac239b61ccae06678
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-30 13:49:29 +1000

    Edit find_smallest function to return index of the first instance of smallest element in list

--- week02/task2.py
+++ week02/task2.py
@@ -1,4 +1,4 @@
-def find_smallest(data_list):
+def find_smallest_index(data_list):
     """
     Find the smallest element in a list
     :param data_list: a list of elements
@@ -7,8 +7,13 @@ def find_smallest(data_list):
     if not data_list:  # Check for empty list
         return None
     smallest = data_list[0]
-    for i in data_list:
-        if i < smallest:
-            smallest = i
-    return smallest
+    for i, number in enumerate(data_list):
+        if number < smallest:
+            smallest = number
+            index = i
+    return index
     # Algorithm runs in O(n)
+
+
+print(find_smallest_index([1, 2, -8, 0, 5, 6, 10, 0]))  # outputs: 2
+print(find_smallest_index([1, 2, 3, 0, 4, 5, 6, 7, 8, 0, 9, 10]))  # outputs: 3
