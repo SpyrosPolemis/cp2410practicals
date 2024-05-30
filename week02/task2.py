18efcf9740ac90ac5f8e4cc6617047f4ae5cfe39
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-30 13:55:05 +1000

    Write tests for find_smallest_index function

--- week02/task2.py
+++ week02/task2.py
@@ -1,3 +1,6 @@
+import random
+
+
 def find_smallest_index(data_list):
     """
     Find the smallest element in a list
@@ -7,6 +10,7 @@ def find_smallest_index(data_list):
     if not data_list:  # Check for empty list
         return None
     smallest = data_list[0]
+    index = 0
     for i, number in enumerate(data_list):
         if number < smallest:
             smallest = number
@@ -17,3 +21,6 @@ def find_smallest_index(data_list):
 
 print(find_smallest_index([1, 2, -8, 0, 5, 6, 10, 0]))  # outputs: 2
 print(find_smallest_index([1, 2, 3, 0, 4, 5, 6, 7, 8, 0, 9, 10]))  # outputs: 3
+random_numbers = [random.randint(-100, 100) for i in range(100000)]
+print(find_smallest_index(random_numbers))
+print(random_numbers.index(-100))  # test to see that above tests finds the index of -200
