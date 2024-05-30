9713247b3bc1ecb8af66b8d075febee82eef983c
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-30 13:39:05 +1000

    Write find_smallest function, to find smallest element in a list

--- week02/task1.py
+++ week02/task1.py
@@ -4,3 +4,11 @@ def find_smallest(data_list):
     :param data_list: a list of elements
     :return: smallest element
     """
+    if not data_list:  # Check for empty list
+        return None
+    smallest = data_list[0]
+    for i in data_list:
+        if i < smallest:
+            smallest = i
+    return smallest
+    # Algorithm runs in O(n)
