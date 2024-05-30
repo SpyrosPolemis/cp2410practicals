7c22d708e913e77e86e04cb5221849e1fabc7e9f
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-30 14:36:29 +1000

    Improve style and write time complexity

--- week02/task3.py
+++ week02/task3.py
@@ -1,5 +1,6 @@
 from task2 import find_smallest_index
 
+
 def selection_sort(data_list):
     """
     Sort a list using selection sort. Time complexity: _______
@@ -13,5 +14,8 @@ def selection_sort(data_list):
         smallest_index = find_smallest_index(data_list)
         sorted_list.append(data_list.pop(smallest_index))
     return sorted_list
+    # O(n^2) since it has to go through list twice for each element
+    # for list size, find smallest (go through list n times)
 
-print(selection_sort([5,3,2,1,6]))
+
+print(selection_sort([5, 3, 2, 1, 6]))
