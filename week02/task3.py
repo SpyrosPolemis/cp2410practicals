94652cd3c284e07a48030895e8d1c34f1083c454
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-30 14:33:52 +1000

    Write selection_sort function

---
+++ week02/task3.py
@@ -0,0 +1,17 @@
+from task2 import find_smallest_index
+
+def selection_sort(data_list):
+    """
+    Sort a list using selection sort. Time complexity: _______
+    :param data_list: a list of elements
+    :return: sorted list
+    """
+    if not data_list:
+        return None
+    sorted_list = []
+    for i in range(len(data_list)):
+        smallest_index = find_smallest_index(data_list)
+        sorted_list.append(data_list.pop(smallest_index))
+    return sorted_list
+
+print(selection_sort([5,3,2,1,6]))
