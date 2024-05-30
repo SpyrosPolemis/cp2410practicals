58d8115dd7cf8543f48037678213fe921f92b7b6
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-05-30 14:40:03 +1000

    Fix type error bug by using floor division to get int when finding the middle index of list

--- week02/task4.py
+++ week02/task4.py
@@ -2,7 +2,7 @@ def binary_search(data_list, target):
     low = 0
     high = len(data_list)
     while low <= high:
-        mid = (low + high) / 2
+        mid = (low + high) // 2
         if data_list[mid] < target:
             low = mid + 1
         elif data_list[mid] > target:
